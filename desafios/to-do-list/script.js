09025a012b7387d565bdc50af0b2b34a955e2b58
dudacommits <devmeduarda@gmail.com>
2026-08-24 13:30:50 -0300

    to do list

--- desafios/to-do-list/script.js
+++ desafios/to-do-list/script.js
@@ -1,19 +1,42 @@
 var main = document.querySelector('main')
+var c = 0
+var txtc = document.getElementById('contador')
+var txt = document.getElementById('texto')
 
 function addtask(){
+    c ++
     var task = document.getElementById('task').value
     var newtask = document.createElement('div')
     newtask.classList.add('task')
-    newtask.innerHTML = `<p>${task}</p> <div id="buttons"><input type="button" value="EDITAR" id="editar" class="buttontask"> <input type="button" value="DELETAR" id="deletar" class="buttontask"></div></div>`
+    newtask.innerHTML = `<p><input type="checkbox" name="check" id="check"> ${task}</p> <div id="buttons"><input type="button" value="EDITAR" id="editar" class="buttontask"> <input type="button" value="DELETAR" id="deletar" class="buttontask"></div></div>`
     main.appendChild(newtask)
+    txtc.innerHTML = c
+
+
+    var checked = newtask.querySelector('#check')
+    checked.addEventListener('click', concluido)
 
     var botaodeletar = newtask.querySelector('#deletar')
     botaodeletar.addEventListener('click', deletartask)
 
+    var botaoeditar = newtask.querySelector('#editar')
+    botaoeditar.addEventListener('click', editartask)
 
 }
 
- 
-function deletartask(){
+function concluido(){
+    c --
+    txtc.innerHTML = c
+    if(c==0){
+     txt.innerHTML = 'Parabens! voce concluiu todas as tasks de hoje!'  
+    } 
+    this.parentElement.parentElement.style.backgroundColor = 'rgb(57, 165, 36)'
+}
+
+ function deletartask(){
+    var confirmar = window.confirm('tem certeza que quer excluir essa tarefa?')
+    if (confirmar == true){
         this.parentElement.parentElement.remove()
+    }
+        
     }
