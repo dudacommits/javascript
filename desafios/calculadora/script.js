a7f9bf2271192035cd3cf7aec4d093733eff4e68
dudacommits <devmeduarda@gmail.com>
2026-09-11 16:26:16 -0300

    calculadora

--- desafios/calculadora/script.js
+++ desafios/calculadora/script.js
@@ -1,22 +1,26 @@
-let visor = document.getElementById('operacoes')
-let numeros = document.querySelectorAll('.number')
+let visor = document.getElementById('numesc')
+let ope = document.getElementById('op')
+let botoes = document.querySelectorAll('.button')
+const n = []
 
-numeros.forEach(numero => {
-    numero.addEventListener('click', clicou)
+botoes.forEach(botao => {
+    botao.addEventListener('click', clicou)
 });
-function clicou(event){
-    let numeroclicado = event.target.innerText
-    visor.innerHTML += numeroclicado
+function clicou (event){
+    
+    let btnclicado = event.target.innerText
+
+    if (+btnclicado >= 0 || btnclicado === "."){
+        visor.innerHTML += btnclicado
+        
+    } else if (btnclicado === "C"  || btnclicado === "CE") {
+        visor.innerHTML = ""
+    } else if (btnclicado === "%"){
+        n.push(Number(visor.innerHTML))
+        console.log(n)
+        visor.innerHTML += btnclicado
+        console.log("op:" + btnclicado)
+    }
+    
 }
     
-
-
-    let operadores = document.querySelectorAll('.op')
-    operadores.forEach(operador => {
-        operador.addEventListener('click', operacao)
-    });
-
-    function operacao(event){
-    let sinal = event.target.innerText
-    visor.innerHTML += sinal
-}
